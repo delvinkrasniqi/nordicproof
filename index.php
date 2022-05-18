4bdd6721d35b2579d9e6aebf2e499dc28be8f082
Delvin <delvinn.k@gmail.com>
2022-05-18 13:52:36 +0200

    added slider

--- index.php
+++ index.php
@@ -37,6 +37,52 @@ $events = [
         'location' => 'Hosted from Oslo, Norway'
     ]
     ];
+
+    $partners = [
+        [
+            'image' => 'landspital.jpg',
+            'url' => ''
+        ],
+        [
+            'image' => 'swecare.jpg',
+            'url' => 'https://www.swecare.se/en/ '
+        ],
+        [
+            'image' => 'smile.png',
+            'url' => 'https://www.smileincubator.life/'
+        ],
+        [
+            'image' => 'stavanger-uh.jpg',
+            'url' => 'https://trokit.com'
+        ],
+        [
+            'image' => 'terkko.jpeg',
+            'url' => 'https://trokit.com'
+        ],
+    ];
+
+    $collaborations = [
+        [
+            'image' => 'landspital.jpg',
+            'url' => ''
+        ],
+        [
+            'image' => 'swecare.jpg',
+            'url' => 'https://www.swecare.se/en/ '
+        ],
+        [
+            'image' => 'smile.png',
+            'url' => 'https://www.smileincubator.life/'
+        ],
+        [
+            'image' => 'stavanger-uh.jpg',
+            'url' => 'https://trokit.com'
+        ],
+        [
+            'image' => 'terkko.jpeg',
+            'url' => 'https://trokit.com'
+        ],
+    ]
 ?>
 
 <!DOCTYPE html>
@@ -50,7 +96,7 @@ $events = [
     <!-- Link Swiper's CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 
 <body>
@@ -61,7 +107,7 @@ $events = [
             laborum debitis, commodi dolores itaque dolorem neque.</p>
         <div class="upcoming-events-wrapper">
             <!-- Swiper -->
-            <div class="swiper mySwiper">
+            <div class="swiper">
                 <div class="swiper-wrapper">
 
                     <?php foreach($events as $event){ ?>
@@ -93,8 +139,74 @@ $events = [
                     </div>
                     <?php } ?>
                 </div>
+            </div>
+        </div>
+        <span class="section-divider" style="margin: 100px 0; display:block"></span>
+        <div class="partners-wrapper">
+            <h1 class="text-center">Our Partners</h1>
+            <div class="bottom-border"></div>
+            <p class="text-center">Our network offers easy access to the most renowned test facilities for health and
+                wellbeing products and solutions in the Nordics.</p>
+            <div class="partners-slider-wrapper">
 
-                <div class="swiper-pagination"></div>
+                <!-- Swiper -->
+                <!-- Swiper -->
+                <div class="partners-swiper swiper">
+                    <div class="swiper-wrapper">
+
+                        <?php foreach($partners as $partner){ ?>
+
+                        <div class="swiper-slide">
+                            <a href="<?php echo $partner['url'] != '' ? $partner['url']: '#';?>"
+                                style="color:inherit; text-decoration:none !important">
+                                <div class="partner-box">
+                                    <img src="./partners/<?php echo $partner['image']; ?>" alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <?php } ?>
+                    </div>
+
+                </div>
+                <!-- Swiper -->
+                <div class="partners-button-next custom-slider-button-next"><img src="./chevron.svg" alt=""
+                        class="slider-button"></div>
+                <div class="partners-button-prev custom-slider-button-prev"><img src="./chevron.svg" alt=""
+                        class="slider-button"></div>
+            </div>
+        </div>
+        <span class="section-divider" style="margin: 100px 0; display:block"></span>
+        <div class="partners-wrapper">
+            <h1 class="text-center">Collaboration Network</h1>
+            <div class="bottom-border"></div>
+            <p class="text-center">Our network offers easy access to the most renowned test facilities for health and
+                wellbeing products and solutions in the Nordics.</p>
+            <div class="collaborations-slider-wrapper">
+
+                <!-- Swiper -->
+                <!-- Swiper -->
+                <div class="collaborations-swiper swiper">
+                    <div class="swiper-wrapper">
+
+                        <?php foreach($collaborations as $collaboration){ ?>
+
+                        <div class="swiper-slide">
+                            <a href="<?php echo $collaboration['url'] != '' ? $collaboration['url']: '#';?>"
+                                style="color:inherit; text-decoration:none !important">
+                                <div class="partner-box">
+                                    <img src="./partners/<?php echo $collaboration['image']; ?>" alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <?php } ?>
+                    </div>
+
+                </div>
+                <!-- Swiper -->
+                <div class="collaborations-button-next custom-slider-button-next"><img src="./chevron.svg" alt=""
+                        class="slider-button"></div>
+                <div class="collaborations-button-prev custom-slider-button-prev"><img src="./chevron.svg" alt=""
+                        class="slider-button"></div>
             </div>
         </div>
     </div>
@@ -111,7 +223,72 @@ $events = [
         loop: true,
         breakpoints: {
             320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                
+            },
+            480: {
                 slidesPerView: 2,
+                spaceBetween: 20
+            },
+            760: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            990: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
+    });
+    const partnerSlider = new Swiper('.partners-swiper', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        // centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".partners-button-next",
+            prevEl: ".partners-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            760: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            990: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
+    });
+
+    const collaborationSlider = new Swiper('.collaborations-swiper', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        // centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+        },
+        navigation: {
+            nextEl: ".collaborations-button-next",
+            prevEl: ".collaborations-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
                 spaceBetween: 10
             },
             480: {
